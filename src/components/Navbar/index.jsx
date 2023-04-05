13964eac9ed2a44f71859cbaa0d48fcb9ffbb14d
mulyan <96921234+mulyant20@users.noreply.github.com>
2023-04-06 05:40:51 +0700

    feature mulyana detail page (#2)
    * feat: add custom hook useFetch and update env
    
    * feat: detail page and fetch by id, styling navbar, detail page, and requests utils
    
    * feat: update detail
    
    * feat: add icon search
    
    * feat: update image
    
    * feat: add skeleton lib at loading

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { BsSearch } from 'react-icons/bs'
+import styles from './index.module.css'
 
 export default function Navbar() {
+  const navigate = useNavigate()
+  const searchVal = useRef()
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    navigate(`/search/${searchVal?.current.value}`)
+  }
+
   return (
-    <div>Navbar</div>
+    <div className={styles.navWrapper}>
+      <div className={styles.container}>
+        <h1 className={styles.brand}>Movielist</h1>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.textfieldGroup}>
+            <input
+              className={styles.textfield}
+              placeholder='What do you want to watch'
+              ref={searchVal}
+            />
+            <span
+              style={{
+                color: 'white',
+                fontSize: '20px',
+                position: 'absolute',
+                top: '54%',
+                right: '1rem',
+                transform: 'translateY(-50%)',
+              }}
+            >
+              <BsSearch />
+            </span>
+          </div>
+          <button type='submit' hidden></button>
+        </form>
+        <div className={styles.btnWrapper}>
+          <button className={styles.btnLogin}>Login</button>
+          <button className={styles.btnRegister}>Register</button>
+        </div>
+      </div>
+    </div>
   )
 }
