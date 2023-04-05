eec4b461c85032570384ddee93721b8c5547aec8
mulyant20 <mulyan.t20@gmail.com>
2023-04-06 04:11:04 +0700

    feat: update detail

--- src/pages/Detail/index.jsx
+++ src/pages/Detail/index.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom'
 import useFetch from '../../hooks/useFetch'
 import { BASE_URL, BASE_URL_IMAGE } from '../../utils/requests'
 import styles from './index.module.css'
+import { BsStar } from 'react-icons/bs'
 
 export default function Detail() {
   const { id } = useParams()
@@ -14,19 +15,57 @@ export default function Detail() {
   }
 
   if (error) {
-    return <p>{erorr}</p>
+    return <p>error</p>
   }
 
   console.log(data)
 
-  return (
-    <>
-      <div className={styles.bgWrapper}>
-        <img
-          src={BASE_URL_IMAGE + data?.backdrop_path}
-          className={styles.bgImg}
-        />
-      </div>
-    </>
-  )
+  if (data) {
+    return (
+      <>
+        <div className={styles.bgWrapper}>
+          <div className={styles.bgLayerWrapper}>
+            <div className={styles.bgLayerContainer}>
+              <h1 className={styles.title}>
+                {data.original_title ? data.original_title : null}
+              </h1>
+              <p style={{ paddingTop: '10px', color: 'white' }}>
+                {data.tagline && data.tagline}
+              </p>
+              <div className={styles.genresWrapper}>
+                {data?.genres.map(({ id, name }) => (
+                  <span key={id} className={styles.genres}>
+                    {name}
+                  </span>
+                ))}
+              </div>
+              <p className={styles.overview}>{data?.overview}</p>
+              <div style={{ paddingTop: '1rem', display: 'flex', gap: '10px' }}>
+                <span className={styles.star}>
+                  <BsStar />
+                </span>
+                <p style={{ color: 'white' }}>
+                  {Math.floor(data.vote_average)} / 10
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              background: 'rgba(0,0,0,0.5)',
+              position: 'absolute',
+              top: '0',
+              left: '0',
+              height: '100%',
+              width: '100%',
+            }}
+          ></div>
+          <img
+            src={BASE_URL_IMAGE + data?.backdrop_path}
+            className={styles.bgImg}
+          />
+        </div>
+      </>
+    )
+  }
 }
