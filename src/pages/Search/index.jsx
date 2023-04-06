2b756baa2c88d95579bafae213bb0042f1ded7aa
Aldi Fahrezi <126686349+Alfarezi1720@users.noreply.github.com>
2023-04-06 08:24:30 +0700

    setup routes and layout

--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 
-export default function Search() {
+function Search() {
+  const [query, setQuery] = useState('')
+  const history = useHistory()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    history.push(`/search/${query}`)
+  }
+
   return (
-    <div>Search</div>
+    <div>
+      <h1>Search Movies</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Search movies...'
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+        />
+        <button type='submit'>Search</button>
+      </form>
+    </div>
   )
 }
+
+export default Search
