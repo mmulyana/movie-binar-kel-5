f99cdbf03fa733f688f6d28759bd150e9065acf2
mulyan <96921234+mulyant20@users.noreply.github.com>
2023-04-07 02:29:01 +0700

    Update MainRoutes.jsx

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -5,10 +5,10 @@ import { Detail, Home, Search } from '../pages'
 export default function MainRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<BaseLayout />}>
-        <Route index element={<Home />} />
-        <Route path="search" element={<Search />} />
-        <Route path="detail/:id" element={<Detail />} />
+      <Route element={<BaseLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/detail/:id" element={<Detail />} />
       </Route>
     </Routes>
   )
