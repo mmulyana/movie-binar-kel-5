b5858b639b4677ff7f42d6dc57d1d574d3f94b46
mulyan <96921234+mulyant20@users.noreply.github.com>
2023-04-07 02:33:07 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import MainRoutes from './routes/MainRoutes'
 
 function App() {
-  const [hasAuth, setHasAuth] = useState(true)
+//   const [hasAuth, setHasAuth] = useState(true)
 
-  return <BrowserRouter>{hasAuth ? <MainRoutes /> : null}</BrowserRouter>
+  return (
+    <BrowserRouter>
+      <MainRoutes />
+    </BrowserRouter>
+  )
 }
 
 export default App
