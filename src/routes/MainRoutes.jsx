35c2d567f1490d12c7b23e33d54536ce7cf4eda5
mulyant20 <mulyan.t20@gmail.com>
2023-04-09 14:08:27 +0700

    update

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,15 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
-import { BaseLayout } from '../components'
 import { Detail, Home, Search } from '../pages'
 
 export default function MainRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<BaseLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/detail/:id" element={<Detail />} />
-      </Route>
+      <Route path='/' element={<Home />} />
+      <Route path='/search/:param' element={<Search />} />
+      <Route path='/detail/:id' element={<Detail />} />
     </Routes>
   )
 }
