5c9192743287f22823c1d8ec8da46eb51ac71383
mulyant20 <mulyan.t20@gmail.com>
2023-04-09 13:58:23 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './assets/css/index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Detail, Home, Search } from './pages'
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/search/:search', element: <Search /> },
+  { path: '/detail/:idMovie', element: <Detail /> },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
