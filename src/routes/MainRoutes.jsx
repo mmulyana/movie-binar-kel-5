41cec2096072a0fd311f98ffef8998afd775421a
mulyant20 <mulyan.t20@gmail.com>
2023-04-09 13:36:50 +0700

    feat: change import detail

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
-import { Detail, Home, Search } from '../pages'
+import { Home, Search } from '../pages'
+import Detail from '../pages/Detail'
 
 export default function MainRoutes() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/search/:search' element={<Search />} />
-      <Route path='/detail' element={<Detail />} />
       <Route path='/detail/:id' element={<Detail />} />
     </Routes>
   )
