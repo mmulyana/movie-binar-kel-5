3f8fac84052389a1c9150c15bc0cfedbd8e8d0d3
mulyant20 <mulyan.t20@gmail.com>
2023-04-09 23:09:19 +0700

    feat: change bg color when offset > 0

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { BsSearch } from 'react-icons/bs'
 import styles from './index.module.css'
@@ -7,13 +7,22 @@ export default function Navbar() {
   const navigate = useNavigate()
   const searchVal = useRef()
 
+  const [offset, setOffset] = useState(0)
+
+  useEffect(() => {
+    const onScroll = () => setOffset(window.pageYOffset)
+    window.removeEventListener('scroll', onScroll)
+    window.addEventListener('scroll', onScroll, { passive: true })
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
+
   function handleSubmit(e) {
     e.preventDefault()
     navigate(`/search/${searchVal?.current.value}`)
   }
 
   return (
-    <div className={styles.navWrapper}>
+    <div className={offset > 0 ? styles.navWrapperActive : styles.navWrapper}>
       <div className={styles.container}>
         <h1 className={styles.brand}>Movielist</h1>
         <form onSubmit={handleSubmit}>
