3f903478a2c6a51a8b539ad56a6bbe448d084c69
mulyan <96921234+mulyant20@users.noreply.github.com>
2023-04-11 05:16:21 +0700

    Feature mulyana detail - review (#7)
    * feat: add custom hook useFetch and update env
    
    * feat: detail page and fetch by id, styling navbar, detail page, and requests utils
    
    * feat: update detail
    
    * feat: add icon search
    
    * feat: update image
    
    * feat: add skeleton lib at loading
    
    * feat: reviews and getRequestURL func
    
    * feat: change bg color when offset > 0
    
    * feat: add MediaQuery for desktop and mobile
    
    * style: review box
    
    * style: change skeleton style

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,47 +1,70 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { BsSearch } from 'react-icons/bs'
 import styles from './index.module.css'
+import MediaQuery from 'react-responsive'
 
 export default function Navbar() {
   const navigate = useNavigate()
   const searchVal = useRef()
+
+  const [offset, setOffset] = useState(0)
+  const [isOpenSearch, setIsOpenSearch] = useState(false)
+
+  useEffect(() => {
+    const onScroll = () => setOffset(window.pageYOffset)
+    window.removeEventListener('scroll', onScroll)
+    window.addEventListener('scroll', onScroll, { passive: true })
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [])
 
   function handleSubmit(e) {
     e.preventDefault()
     navigate(`/search/${searchVal?.current.value}`)
   }
 
+  function handleOpenSearch() {
+    setIsOpenSearch(!isOpenSearch)
+  }
+
   return (
-    <div className={styles.navWrapper}>
+    <div className={offset > 0 ? styles.navWrapperActive : styles.navWrapper}>
       <div className={styles.container}>
-        <h1 className={styles.brand}>Movielist</h1>
-        <form onSubmit={handleSubmit}>
-          <div className={styles.textfieldGroup}>
-            <input
-              className={styles.textfield}
-              placeholder='What do you want to watch'
-              ref={searchVal}
-            />
-            <span
-              style={{
-                color: 'white',
-                fontSize: '20px',
-                position: 'absolute',
-                top: '54%',
-                right: '1rem',
-                transform: 'translateY(-50%)',
-              }}
-            >
-              <BsSearch />
-            </span>
+        <MediaQuery minWidth={786}>
+          <h1 className={styles.brand}>Movielist</h1>
+          <form onSubmit={handleSubmit}>
+            <div className={styles.textfieldGroup}>
+              <input
+                className={styles.textfield}
+                placeholder='What do you want to watch'
+                ref={searchVal}
+              />
+              <span
+                style={{
+                  color: 'white',
+                  fontSize: '20px',
+                  position: 'absolute',
+                  top: '54%',
+                  right: '1rem',
+                  transform: 'translateY(-50%)',
+                }}
+              >
+                <BsSearch />
+              </span>
+            </div>
+            <button type='submit' hidden></button>
+          </form>
+          <div className={styles.btnWrapper}>
+            <button className={styles.btnLogin}>Login</button>
+            <button className={styles.btnRegister}>Register</button>
           </div>
-          <button type='submit' hidden></button>
-        </form>
-        <div className={styles.btnWrapper}>
-          <button className={styles.btnLogin}>Login</button>
-          <button className={styles.btnRegister}>Register</button>
-        </div>
+        </MediaQuery>
+        <MediaQuery maxWidth={768}>
+          <h1 className={styles.brand}>Movielist</h1>
+          <button onClick={handleOpenSearch} className={styles.btnOpenSearch}>
+            <BsSearch />
+          </button>
+        </MediaQuery>
       </div>
     </div>
   )
