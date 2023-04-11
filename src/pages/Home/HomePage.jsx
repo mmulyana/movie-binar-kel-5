599565d1bf2238dfdb62ce64f857bcede76481c4
mulyant20 <mulyan.t20@gmail.com>
2023-04-11 12:17:59 +0700

    style: update

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import styles from './HomePage.module.css'
-import useFetch from '../../hooks/useFetch'
+import useFetch from '../WHA../hooks/useFetch'
 import { BASE_URL_IMAGE, getRequestURL } from '../../utils/requests'
 import { Carousel } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { AiOutlineArrowRight } from 'react-icons/ai'
+import Skeleton from 'react-loading-skeleton'
+import { BaseLayout } from '../../components'
+
+
 function HomePage() {
-  const { data, error, loading } = useFetch(getRequestURL('upcoming'))
+  const { data } = useFetch(getRequestURL('upcoming'))
 
   if (!data) {
-    return <p>Loading</p>
+    return (
+      <BaseLayout>
+        <Skeleton style={{ height: '800px' }} />
+      </BaseLayout>
+    )
   }
 
   return (
-    <div>
-      <Carousel>
+    <BaseLayout>
+      <Carousel style={{height: '80vh', overflow: 'hidden', zIndex: '-1'}}>
         {data.results.slice(0, 3).map((data) => (
           <Carousel.Item>
             <img
@@ -25,9 +33,6 @@ function HomePage() {
             <div style={{ position: 'absolute', left: '0', zIndex: '2' }}>
               <Carousel.Caption>
                 <h3>{data.original_title}</h3>
-                <p>
-                  Nulla vitae elit libero, a pharetra augue mollis interdum.
-                </p>
               </Carousel.Caption>
             </div>
           </Carousel.Item>
@@ -42,14 +47,23 @@ function HomePage() {
             </Link>
           </button>
         </div>
-        {data.results.slice(0, 5).map((data, index) => (
-          <div key={index}>
-            <h4>{data.title}</h4>
-            <img src={BASE_URL_IMAGE + data?.backdrop_path} alt='First slide' />
-          </div>
-        ))}
+        <div className={styles.cardMovieWrapper}>
+          {data.results.slice(0, 5).map((data, index) => (
+            <div
+              key={index}
+              className={styles.cardItem}
+              style={{
+                backgroundImage: `url(${BASE_URL_IMAGE + data?.backdrop_path})`,
+              }}
+            >
+              <div className={styles.cardBody}>
+                <h4>{data.title}</h4>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </BaseLayout>
   )
 }
 
